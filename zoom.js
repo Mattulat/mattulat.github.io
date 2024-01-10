6c36c3b180d3a57eef4bc78705a3dcd14413ef35
ParaMattKoopa <119807878+ParaMattKoopa@users.noreply.github.com>
2024-01-10 09:45:17 -0500

    Update zoom.js

--- zoom.js
+++ zoom.js
@@ -3,6 +3,7 @@
 $(document).ready(function(){
 	$(document).keydown(function(event) {
 	    	    if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+			    alert('sorry but zooming breaks page setup !!!'); 
 		event.preventDefault();
 		// 107 Num Key  +
 		//109 Num Key  -
